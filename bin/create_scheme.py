c030d1ef51592865e16f85f1f07a759874775f86
Krivosheev Ivan <i.krivosheev@rambler-co.ru>
2017-04-02 19:15:12 +0300

    Fix create scheme

--- bin/create_scheme.py
+++ bin/create_scheme.py
@@ -5,7 +5,6 @@ if __name__ == '__main__':
     import sys
     '.' not in sys.path and sys.path.insert(0, '.')
 
-    from shop.db import import_all
-    from shop.db import Base
-    import_all()
-    Base.metadata.create_all()
+    from shop import db
+    db.import_all()
+    db.Base.metadata.create_all(db.engine)
