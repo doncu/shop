c4763bc639fa41a4d282b9b8fba1fe3474deab2f
Krivosheev Ivan <i.krivosheev@rambler-co.ru>
2017-04-23 20:32:19 +0300

    Fix text

--- shop/bot.py
+++ shop/bot.py
@@ -22,12 +22,17 @@ class TeleBot:
 
 def send_message(name, email, phone, description, basket):
     basket_text = ''
+    for i, obj in enumerate(basket):
+        prod, count = obj
+        obj_str = '{index}. {name} кол-во: {count}\n'.format(index=i+1, name=prod.title, count=count)
+        basket_text += obj_str
     text = '''Вам поступил заказ:
     Имя: {name}
     Email: {email}
     Телефон: {phone}
     Описание: {description}
-    Заказ: {basket}
+    Заказ:
+    {basket}
     '''.format(name=name, email=email, phone=phone, description=description, basket=basket_text)
     bot.send_message(text)
 
